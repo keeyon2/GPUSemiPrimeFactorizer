412f3b361d7eae2e97d853772e70cc629fa266f1
I <Keeyon@172-16-180-205.DYNAPOOL.NYU.EDU>
2015-12-09 15:16:36 -0500

    Added tests

---
+++ runTests.sh
@@ -0,0 +1,35 @@
+(time ./sequential 6) &>> results
+(time ./GPU 6) &>> results
+
+(time ./sequential 1689303271703) &>> results
+(time ./GPU 1689303271703) &>> results
+
+(time ./sequential 7563413027531) &>> results
+(time ./GPU 7563413027531) &>> results
+
+(time ./sequential 18115689599993) &>> results
+(time ./GPU 18115689599993) &>> results
+
+(time ./sequential 33641728422101) &>> results
+(time ./GPU 33641728422101) &>> results
+
+(time ./sequential 54299228177501) &>> results
+(time ./GPU 54299228177501) &>> results
+
+(time ./sequential 80290058462759) &>> results
+(time ./GPU 80290058462759) &>> results
+
+(time ./sequential 111743081139479) &>> results
+(time ./GPU 111743081139479) &>> results
+
+(time ./sequential 148724512810747) &>> results
+(time ./GPU 148724512810747) &>> results
+
+(time ./sequential 191383152856891) &>> results
+(time ./GPU 191383152856891) &>> results
+
+(time ./sequential 239811859939591) &>> results
+(time ./GPU 239811859939591) &>> results
+
+(time ./sequential 12) &>> results
+(time ./GPU 12) &>> results
